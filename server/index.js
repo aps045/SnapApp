4131c60b8ab824d6101a79da128edd502ffbfc85
Aditya Pratap Singh <aditya.singh1_cs18@gla.ac.in>
2021-05-09 03:28:43 +0530

    some more changes

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 
 
 const app = express();
-dotenv.config();
+// dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -19,9 +19,10 @@ app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 // const CONNECTION_URL = "mongodb+srv://newuser123:newuser0123@cluster0.zq1l2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = "mongodb://rootuseradmin045:123456@aps045-snapapp-mongo-production/admin";
+const PORT = 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {
+mongoose.connect(CONNECTION_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`)))
