1e494a0692a1a5f407aea5fa2fcf4592a190d717
Aditya Pratap Singh <aditya.singh1_cs18@gla.ac.in>
2021-05-09 03:40:00 +0530

    some more changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 
-// const API = axios.create({ baseURL: 'http://localhost:5000' });
-const API = axios.create({ baseURL: 'https://aps045-snapapp-backend.zeet.app/' });
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
